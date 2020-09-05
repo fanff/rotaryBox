d3f2cf2b44bcbccf8e7ab5d2b04c41ef9528ab0f
fanf <fanf@fanf>
2020-09-05 07:37:35 +0200

    Update rotary2.ino

--- rotary2/rotary2.ino
+++ rotary2/rotary2.ino
@@ -32,7 +32,7 @@ int lastbtn[50];
 int BUTTONSHIFT=0;
 
 
-//volatile long temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
+volatile long temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
 
 //#define DEBUGSERIAL
 void setup() {
@@ -53,14 +53,75 @@ void setup() {
   pinMode(A1, INPUT_PULLUP);
   pinMode(A2, INPUT_PULLUP);
   pinMode(A3, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
   
   pinMode(0, INPUT);
-  pinMode(1, INPUT_PULLUP);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(4, INPUT);
+  pinMode(9, INPUT);
 
+  //0, 1, 2, 3, 7
+  attachInterrupt(digitalPinToInterrupt(1), ai1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(2), ai2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(3), ai3, FALLING);
+  attachInterrupt(digitalPinToInterrupt(7), ai4, FALLING);
+}
 
+volatile byte latchinput = 0;
+byte safelatchinput = 0;
+byte rotmoving = 0;
+
+void ai1() {
+  rotmoving=0;
+  if (latchinput==0){
+    if(digitalRead(5)==LOW ) {
+      temp=1;
+      rotmoving+=1;
+    }else{
+      temp=-1;
+    }
+  }
+}
+void ai2() {
+  // ai0 is activated if DigitalPin nr 2 is going from HIGH to LOW 
+  // Check pin 3 to determine the direction
+  rotmoving=2;
+  if (latchinput==0){
+    if(digitalRead(6)==LOW ) {
+      temp=1;
+      rotmoving+=1;
+    }else{
+      temp=-1;
+    }
+  }
 }
 
 
+
+void ai3() {
+  rotmoving=4;
+  if (latchinput==0){
+    if(digitalRead(4)==LOW ) {
+      temp=1;
+      rotmoving+=1;
+    }else{
+      temp=-1;
+    }
+  }
+}
+
+void ai4() {
+  rotmoving=6;
+  if (latchinput==0){
+    if(digitalRead(9)==LOW ) {
+      temp=1;
+      rotmoving+=1;
+    }else{
+      temp=-1;
+    }
+  }
+}
 
 void setButton(int bID,int val){
   if(lastbtn[bID] != val){
@@ -83,12 +144,13 @@ void buttCheck(int pin,int bID){
     setButton(bID+BUTTONSHIFT, 0);
   }else{
     setButton(bID+BUTTONSHIFT, 1);
+    
   }
 }
 
 int modeCheck(){
   int mod = digitalRead(0);
-  Joystick.setButton(17, mod);
+  setButton(17, mod);
   if(mod==0) {
     return 0;
   }else{
@@ -96,11 +158,79 @@ int modeCheck(){
   }
   
 }
-void loop() {
 
+
+void loop() {
+  // Send the value of counter
+  noInterrupts();
+    // critical, time-sensitive code here
+    counter=temp;
+    if(counter>0 ){
+
+      latchinput = 1;
+      safelatchinput=1;
+      #ifdef DEBUGSERIAL
+
+        Serial.print(millis());
+
+        Serial.print(" pot: " );
+        Serial.print(rotmoving+BUTTONSHIFT);
+        Serial.print("->>>" );
+
+
+        
+      #else
+        setButton(rotmoving+BUTTONSHIFT, 1);
+      #endif
+    }else if(counter<0){
+      latchinput = 1;
+      safelatchinput=1;
+      #ifdef DEBUGSERIAL
+        Serial.print(millis());
+
+        Serial.print(" pot: " );
+        Serial.print(rotmoving+BUTTONSHIFT);
+        Serial.print("<<<---  " );
+
+        
+      #else
+        setButton(rotmoving+BUTTONSHIFT, 1);
+      #endif
+    }
+
+    // in case buttons does latch, prevent pot to switch
+    latchinput = safelatchinput;
+    temp=0;
+    
+  interrupts();
+
+  if(safelatchinput==1){
+    delay(10); // stop for a moment debounce
+    
+    noInterrupts();
+    latchinput = 0;
+    safelatchinput = 0;
+    interrupts();
+    
+    #ifdef DEBUGSERIAL
+      Serial.print("unlatch" );
+      Serial.println("" );
+    #else
+      for(byte i=18;i<=25;i++){
+        setButton(i, 0);  
+        delay(2);
+      }
+      for(byte i=0;i<=7;i++){
+        setButton(i, 0);  
+        delay(2);
+      }
+      
+    #endif
+  }
+  
   BUTTONSHIFT = modeCheck();
 
-  buttCheck(1,12);
+  buttCheck(8,12);
   buttCheck(A0,13);
   buttCheck(A1,14);
   buttCheck(A2,15);
